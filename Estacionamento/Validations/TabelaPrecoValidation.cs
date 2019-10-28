20392e201bb134644a7604e095a097bcda0a61c6
Eduardo Rodrigues de Aguiar <eduardo.aguiar@orthos.inf.br>
2019-10-28 19:16:58 -0300

    Mais um passo

---
+++ Estacionamento/Validations/TabelaPrecoValidation.cs
@@ -0,0 +1,31 @@
+﻿using Estacionamento.Core;
+using Estacionamento.Entities;
+using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estacionamento.Validations
+{
+    public class TabelaPrecoValidation: BaseValidation<TabelaPreco>
+    {
+        public TabelaPrecoValidation()
+        {
+            RuleFor(v => v).Custom((t, context) => 
+            {
+                if (t.Inicio > t.Fim)
+                {
+                    context.AddFailure(new ValidationFailure(nameof(t.Inicio), "Data de Início não pode ser maior que a data final!"));
+                    context.AddFailure(new ValidationFailure(nameof(t.Fim), "Data de Início não pode ser maior que a data final!"));
+                }
+            });
+            RuleFor(v => v.Preco).LessThan(0).WithMessage("Preço não pode ser menor ou igual a zero!");
+            RuleFor(v => v.Preco).Equal(0).WithMessage("Preço não pode ser menor ou igual a zero!");
+            RuleFor(v => v.ToleranciaMinutos).LessThan(0).WithMessage("Tolerancia não pode ser menor ou igual a zero!");
+            RuleFor(v => v.ToleranciaMinutos).Equal(0).WithMessage("Tolerancia não pode ser menor ou igual a zero!");
+
+        }
+    }
+}
---
+++ Estacionamento/ViewModel/MovimentacaoViewModel.cs
@@ -0,0 +1,75 @@
+﻿using Estacionamento.Entities;
+using Estacionamento.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estacionamento.ViewModel
+{
+    public class MovimentacaoViewModel
+    {
+        private bool _editMode;
+        private readonly ITabelaPrecosService _tabelaPrecoService;
+        public MovimentacaoViewModel(ITabelaPrecosService tabelaPrecosService, bool editMode)
+        {
+            _tabelaPrecoService = tabelaPrecosService;
+            _editMode = editMode;
+        }
+
+        public DateTime DataEntrada { get; set; }
+
+        private DateTime? _dataSaida;
+        public DateTime? DataSaida 
+        { 
+            get
+            {
+                return _dataSaida;
+            }
+            set
+            {
+                _dataSaida = value;
+                CalculePermanencia();
+            }
+        }
+
+        private void CalculePermanencia()
+        {
+
+
+            if (!_editMode)
+                return;
+            if (!_dataSaida.HasValue)
+            {
+                Permanencia = 0;
+            }
+
+            /*
+             *         
+        DateTime inicio = new DateTime(2019, 10, 28, 18, 26, 35);
+        DateTime final = new DateTime(2019, 10, 28, 22, 37, 45);
+
+        TimeSpan teste = final - inicio;
+        double testevalue = teste.TotalMinutes;
+		Console.WriteLine(testevalue);
+		
+		var qtHoras = Math.Truncate(testevalue /60); 
+		Console.WriteLine(qtHoras);
+		var resto = testevalue % 60;
+	    Console.WriteLine(resto);
+
+             * 
+             */
+
+
+            TimeSpan diferenca = _dataSaida.Value - DataEntrada;
+            Permanencia = diferenca.TotalMinutes;
+        }
+
+        public string Placa { get; set; }
+        public string Descricao { get; set; }
+        public double Permanencia { get; private set; }
+        public decimal? ValorTotal { get; }
+
+    }
+}
