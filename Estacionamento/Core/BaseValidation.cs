cc911919c7d9ccc853ebebafc0cefb7471d428c5
Eduardo Aguiar <eduaguiar281@hotmail.com>
2019-10-27 23:31:22 -0300

    Validation

---
+++ Estacionamento/Core/BaseValidation.cs
@@ -0,0 +1,20 @@
+﻿using Estacionamento.DataBase.DataContext;
+using Estacionamento.Entities.Base;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Estacionamento.Core
+{
+    public class BaseValidation<TModel>: AbstractValidator<TModel> where TModel : class
+    {
+        public BaseValidation()
+        {
+
+        }
+
+    }
+}
